5f66fd996533eafc384484cf39ee94dbea4e1fee
Yuki Sako <divine_wind_820@yahoo.co.jp>
2017-09-08 01:28:05 +0000

    add seed file to create admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(name: 'admin',email: 'admin@example.com',password: 'admin',password_confirmation: 'admin', is_admin: 'true')
